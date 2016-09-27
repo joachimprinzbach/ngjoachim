3fdd646cd4957d74f443fb4c5df6576e3b1142cb
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2016-09-27 11:41:38 +0200

    Implement basic watch and digest.

--- src/scope.js
+++ src/scope.js
@@ -1,3 +1,18 @@
 export class Scope {
 
+    constructor() {
+        this.$$watchers = [];
+    }
+
+    $watch(watchFunc, listenerFunc) {
+        let watcher = {
+            watchFunc: watchFunc,
+            listenerFunc: listenerFunc
+        };
+        this.$$watchers.push(watcher);
+    }
+
+    $digest() {
+        this.$$watchers.forEach(watcher => watcher.listenerFunc());
+    }
 }
--- src/scope.spec.js
+++ src/scope.spec.js
@@ -1,10 +1,28 @@
 import {Scope} from './scope';
 
-describe('scope', function () {
-    it('should be constructable as an object', function () {
-        var scope = new Scope();
+describe('scope', () => {
+    it('should be constructable as an object', () => {
+        let scope = new Scope();
         scope.prop = 12;
 
         expect(scope.prop).toEqual(12);
-    })
+    });
+
+    describe('digest', () => {
+        let scope;
+
+        beforeEach(() => {
+           scope = new Scope();
+        });
+
+        it('should call the listener function of watch', () => {
+            let watchFunc = () => 'watch';
+            let listenerFunc = jasmine.createSpy();
+            scope.$watch(watchFunc, listenerFunc);
+
+            scope.$digest();
+
+            expect(listenerFunc).toHaveBeenCalled();
+        });
+    });
 });
