de9561ed4e2b74e76f1dd3d702a559a56aa3308f
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-09-27 10:31:34 +0200

    Add first test for scope.

---
+++ src/scope.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+describe('scope', () => {
+
+    it('can be constructed and used as an object', () => {
+        var scope = new Scope();
+        scope.someProp = 11;
+
+        ecpect(scope.someProp).toEqual(11);
+    })
+})
